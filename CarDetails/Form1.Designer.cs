8efb63668191955f845f96c45f9e4c74f9691afb
Mugesh Shanmugavel <mugesh.shanmugavel@justanswer.com>
2021-12-10 20:23:55 +0530

    Addec Car details projects

---
+++ CarDetails/Form1.Designer.cs
@@ -0,0 +1,357 @@
+﻿namespace CarDetails
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(48, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Customer Details";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(51, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Customer ID";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(51, 121);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Customer Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(55, 273);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Address";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(55, 409);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(78, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Phone Number";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(55, 358);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(64, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Postal Code";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(165, 59);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(181, 20);
+            this.textBox1.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(165, 121);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(181, 20);
+            this.textBox2.TabIndex = 8;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(165, 273);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(181, 59);
+            this.textBox3.TabIndex = 9;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(165, 358);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(181, 20);
+            this.textBox4.TabIndex = 10;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(165, 409);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(181, 20);
+            this.textBox5.TabIndex = 11;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(165, 462);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(271, 462);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Update";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(215, 579);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "Reset";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(165, 525);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 15;
+            this.button4.Text = "Delete";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(165, 200);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(48, 17);
+            this.radioButton1.TabIndex = 16;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Male";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(271, 200);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(59, 17);
+            this.radioButton2.TabIndex = 17;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Female";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(55, 202);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(42, 13);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "Gender";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(418, 567);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(116, 23);
+            this.button5.TabIndex = 19;
+            this.button5.Text = "First";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(573, 567);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(116, 23);
+            this.button6.TabIndex = 20;
+            this.button6.Text = "Prev";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(736, 567);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(104, 23);
+            this.button7.TabIndex = 21;
+            this.button7.Text = "Next";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(877, 567);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(103, 23);
+            this.button8.TabIndex = 22;
+            this.button8.Text = "Last";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(418, 59);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(562, 452);
+            this.dataGridView1.TabIndex = 23;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(685, 645);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(75, 23);
+            this.button9.TabIndex = 24;
+            this.button9.Text = "Get";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(271, 525);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(75, 23);
+            this.button10.TabIndex = 25;
+            this.button10.Text = "Search";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1001, 771);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+    }
+}
+
---
+++ CarDetails/Form1.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CarDetails
+{
+    public partial class Form1 : Form
+    {
+        CarEntities db;
+        public Form1()
+        {
+            InitializeComponent();
+            db = new CarEntities();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var newCar = new Car_Details
+            {
+                Id = Convert.ToInt32(textBox1.Text),
+                Name = textBox2.Text,
+                Gender = radioButton1.Checked ? "Male" : "Female",
+                Address = textBox3.Text,
+                PostalCode = textBox4.Text,
+                PhoneNumber = textBox5.Text
+            };
+            db.Car_Details.Add(newCar);
+            db.SaveChanges();
+            MessageBox.Show("Customer added: " + textBox2.Text);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var cars = db.Car_Details.ToList();
+
+            if (cars.Count == 0)
+            {
+                textBox1.Text = "1";
+            }
+            else
+            {
+                textBox1.Text = Convert.ToString(cars.Count() + 1);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var cars = db.Car_Details.ToList();
+            int index = 0;
+            foreach(var car in cars)
+            { 
+                if(car.Id == Convert.ToInt32(textBox1.Text))
+                {
+                        
+                    car.Name = textBox2.Text;
+                    car.Gender = radioButton1.Checked ? "Male" : "Female";
+                    car.Address = textBox3.Text;
+                    car.PostalCode = textBox4.Text;
+                    car.PhoneNumber = textBox5.Text;
+                    db.Car_Details.ToList()[index] = car;
+                    db.SaveChanges();
+                    MessageBox.Show("Customer Updated: " + car.Id);
+                    break;
+                }
+                index++;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text != string.Empty)
+            {
+                var cars = db.Car_Details.ToList();
+                int index = 0;
+                foreach (var car in cars)
+                {
+                    if (car.Id == Convert.ToInt32(textBox1.Text))
+                    {
+                        db.Car_Details.ToList().RemoveAt(index);
+                        db.SaveChanges();
+                        MessageBox.Show("Customer Deleted: " + car.Id);
+                        break;
+                    }
+                    index++;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please enter Id...");
+            }
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != string.Empty)
+            {
+                var cars = db.Car_Details.ToList();
+                foreach (var car in cars)
+                {
+                    if (car.Id == Convert.ToInt32(textBox1.Text))
+                    {
+                        textBox2.Text = car.Name;
+                        if(car.Gender == "Male")
+                        {
+                            radioButton1.Checked = true;
+                        }
+                        else
+                        {
+                            radioButton2.Checked = true;
+                        }
+                        textBox3.Text = car.Address;
+                        textBox4.Text = car.PostalCode;
+                        textBox5.Text = car.PhoneNumber;
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please enter Id...");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = string.Empty;
+            textBox3.Text = string.Empty;
+            textBox4.Text = string.Empty;
+            textBox5.Text = string.Empty;
+            var cars = db.Car_Details.ToList();
+            textBox1.Text = Convert.ToString(cars.Count() + 1);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            var cars = db.Car_Details.ToList();
+
+            textBox1.Text = Convert.ToString(cars[0].Id);
+            textBox2.Text = cars[0].Name;
+            if (cars[0].Gender == "Male")
+            {
+                radioButton1.Checked = true;
+            }
+            else
+            {
+                radioButton2.Checked = true;
+            }
+            textBox3.Text = cars[0].Address;
+            textBox4.Text = cars[0].PostalCode;
+            textBox5.Text = cars[0].PhoneNumber;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            var cars = db.Car_Details.ToList();
+
+            var lastIndex = cars.Count() - 1;
+            textBox1.Text = Convert.ToString(cars[lastIndex].Id);
+            textBox2.Text = cars[lastIndex].Name;
+            if (cars[lastIndex].Gender == "Male")
+            {
+                radioButton1.Checked = true;
+            }
+            else
+            {
+                radioButton2.Checked = true;
+            }
+            textBox3.Text = cars[lastIndex].Address;
+            textBox4.Text = cars[lastIndex].PostalCode;
+            textBox5.Text = cars[lastIndex].PhoneNumber;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            var cars = db.Car_Details.ToList();
+            if (textBox1.Text != string.Empty)
+            {
+                var prevIndex = Convert.ToInt32(textBox1.Text) - 2;
+                if (prevIndex <= -1)
+                {
+                    MessageBox.Show(" No Prev record found...");
+                }
+                else
+                {
+                    textBox1.Text = Convert.ToString(cars[prevIndex].Id);
+                    textBox2.Text = cars[prevIndex].Name;
+                    if (cars[prevIndex].Gender == "Male")
+                    {
+                        radioButton1.Checked = true;
+                    }
+                    else
+                    {
+                        radioButton2.Checked = true;
+                    }
+                    textBox3.Text = cars[prevIndex].Address;
+                    textBox4.Text = cars[prevIndex].PostalCode;
+                    textBox5.Text = cars[prevIndex].PhoneNumber;
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("Please enter Id...");
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            var cars = db.Car_Details.ToList();
+            if (textBox1.Text != string.Empty)
+            {
+                var nextIndex = Convert.ToInt32(textBox1.Text);
+                if (nextIndex > cars.Count)
+                {
+                    MessageBox.Show(" No Next record found...");
+                }
+                else
+                {
+                    textBox2.Text = cars[nextIndex].Name;
+                    if (cars[nextIndex].Gender == "Male")
+                    {
+                        radioButton1.Checked = true;
+                    }
+                    else
+                    {
+                        radioButton2.Checked = true;
+                    }
+                    textBox3.Text = cars[nextIndex].Address;
+                    textBox4.Text = cars[nextIndex].PostalCode;
+                    textBox5.Text = cars[nextIndex].PhoneNumber;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please enter Id...");
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            var cars = db.Car_Details.ToList();
+            dataGridView1.DataSource = cars;
+        }
+    }
+}
